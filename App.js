723d35fe4bfc832c40c6da66d89be7833e34bbe3
stittpaul2001 <stittpaul2001@gmail.com>
2025-05-07 10:52:41 -0600

    trying to get functions working for the boss and heros

--- App.js
+++ App.js
@@ -20,22 +20,72 @@ const boss = {
   level: 1
 }
 
-function findHeroes() {
-  heroes.forEach(hero => {
-    let heroElement = document.getElementById(hero.name)
-    console.log('Getting Heros')
 
+
+
+//NOTE - Attacking the boss
+
+
+function attackBoss() {
+  let totalDamage = 0
+  heroes.forEach(hero => {
+    if (hero.health == 0)
+      return
+    totalDamage += hero.damage  //... Calculate total damage
+    console.log('attacking Fat-Head-Smurf')
+  })
+  boss.health -= totalDamage
+
+  if (boss.health < 1)  //... Reduce the boss health and update the boss's health on the page
+    boss.health = 0
+
+}
+
+//NOTE - Boss attacking back
+
+
+function bossAttack() {
+  heroes.forEach(hero => {
+    hero.health -= Math.floor(boss.damage * (boss.level * Math.random()))
+    if (hero.health < 1) {
+      hero.health = 0
+      //... Update the hero's health on the page
+
+    }
   })
 }
 
-function drawHeroes(heroName) {
-  const findHeroes = heroes.find(hero => hero.name == heroName)
-  console.log('herosname')
 
-}
+//NOTE - Leveling up
+
+
+// function levelUpBoss() {
+//   if (boss.health <= 0) {
+//     //... Update the boss level and health
+//   }
+// }
+
+
+//NOTE - Getting paid
+
+
+// let heroGold = 0
+// let bank = heroGold
+
+
+// function awardHeroes() {
+//   let gold = 0
+//   heroes.forEach(hero => {
+
+//   })
+// Increase the heroes gold, based on boss level, Update gold display on the page
+
+//NOTE - Heroes Dying
 
 function heroHealth() {
   heroes.forEach(hero => {
     hero.health -= 1
+    // console.log('hero health dropping')
   })
 }
+
