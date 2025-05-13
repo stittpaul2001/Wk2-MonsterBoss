ed3b7e9bfc394c2ca7fd0e2b5c98302457adc424
stittpaul2001 <stittpaul2001@gmail.com>
2025-05-13 15:32:45 -0600

    got functions drawing how i wanted them to

--- App.js
+++ App.js
@@ -17,13 +17,14 @@ const boss = {
   health: 100,
   maxHealth: 100,
   damage: 5,
-  level: 1
+  level: 1,
+  award: 50
 }
 
+let heroGold = 0
 
-
-//NOTE - the logical aspect of the page.
-
+//section - the logical aspect of the page.
+//NOTE - this is my onclick for the boss
 function attackBoss() {
   let totalDamage = 0
   heroes.forEach(hero => {
@@ -32,52 +33,111 @@ function attackBoss() {
   })
 
   boss.health -= totalDamage
+  if (boss.health < 0) {
+    boss.health = 0
+    console.log('boss is dead')
+    levelUpBoss()
+  }
 
   console.log('boss is being attacked')
   console.log('boss health', boss.health)
-  drawBossHealthBar()
-  updateBossHealth()
+  drawBossStats()
+
 }
 
-function updateBossHealth() {
-  if (boss.health < 1) {
-    drawBoss()
-    drawBossHealthBar()
-  }
+
+
+function bossAttack() {
+  heroes.forEach(hero => {
+    if (hero.health == 0) return
+    hero.health -= Math.floor(boss.damage * (boss.level * Math.random()))
+    if (hero.health < 0) {
+      hero.health = 0
+      console.log(hero.name + ' is dead')
+    }
+  })
+  console.log('boss is attacking')
+  checkForDeath()
+  drawHeroes()
+}
+function checkForDeath() {
+  const allHeroesDead = heroes.every(hero => hero.health === 0)
+  if (allHeroesDead) return
+
+  const heroesReadyToFight = window.confirm('Shall we fight again?')
+  if (!heroesReadyToFight) return
+  boss.maxHealth = 100
+  boss.health = boss.maxHealth
+  boss.level = 1
+  drawBossStats()
+  heroes.forEach(hero => {
+    hero.health = 100
+  })
+}
+function levelUpBoss() {
+  if (boss.health === 0)
+    console.log('boss is dead now leveling up')
+  boss.maxHealth = Math.floor(boss.maxHealth * (boss.level * Math.random()))
+  boss.health = boss.maxHealth
+  boss.level++
+  console.log('boss level up')
+  heroGold += Math.ceil(Math.random() * 100)
+  drawHeroGold()
+
 }
 
-let heroGold = 0
-let bank = heroGold;
 
 function awardHeroes() {
-  bank += boss.reward
-  drawBank() // Increase the heroes gold, based on boss level, Update gold display on the page
+  heroGold += boss.award
+  drawHeroGold()
+  console.log('heroes are being awarded')
 }
 
 
 
-//NOTE - Draw section for the boss/heroes/ and the bank
-function drawBank() {
-  const bankElm = document.getElementById('reward')
-  const bankAmount = bankElm.querySelector('bank-amount')
-  bankAmount.innerHTML = 'Gold: ' + bank.toString()
-}
-function drawBoss() {
-  const bossElement = document.getElementById('boss-health')
-  bossElement.innerText = boss.health.toString()
-  const bossMaxHealth = bossElement.querySelector('MaxHealth')
+//Section - Draw section for the boss/heroes/ and the bank
 
-  const bossLevel = bossElement.querySelector('Level')
-  bossLevel.innerHTML = 'Level:' + boss.level.toString()
+function drawHeroGold() {
 
+  const heroGoldElement = document.getElementById('hero-gold')
+  heroGoldElement.innerText = heroGold.toString()
 
 }
 
-function drawBossHealthBar() {
+function drawSlabRockStats() {
+  const slabrockHealthElm = document.getElementById('slabrock-health')
+  const slabrockLevelElm = document.getElementById('slabrock-level')
+  slabrockHealthElm.innerText = heroes[0].health.toString()
+  slabrockLevelElm.innerText = boss.level.toString()
+}
+
+function drawSwiftIronstagStats() {
+  const swiftIronstagHealthElm = document.getElementById('swiftIronstag-health')
+  const swiftIronstagLevelElm = document.getElementById('swiftIronstag-level')
+  swiftIronstagHealthElm.innerText = heroes[1].health.toString()
+  swiftIronstagLevelElm.innerText = boss.level.toString()
+
+}
+function drawHeroes() {
+  drawSlabRockStats()
+  drawSwiftIronstagStats()
+}
+
+function drawBossStats() {
+  const bossHealth = Math.floor((boss.health / boss.maxHealth) * 100)
+  const bossElement = document.getElementById('boss')
+
   const bossBarElement = document.getElementById('boss-health-bar')
   bossBarElement.innerText = boss.health.toString()
+  const bossHealthElement = document.getElementById('bossHealth')
+  bossHealthElement.style.width = bossHealth + '%'
+
+  const bossLevelElement = document.getElementById('bossLevel')
+  bossLevelElement.innerText = boss.level.toString()
+
 }
 
-//NOTE -happens when the page loading
 
-// setInterval()
+//Section -happens when the page loading
+drawHeroes()
+setInterval(bossAttack, 5000)
